7ad9292926006c08af751f20178db3951f4db1cd
DSBB-CR <maximus_p@icloud.com>
2025-03-17 12:36:46 +0300

    Update testProject.cpp
    aaaa

--- testProject/testProject.cpp
+++ testProject/testProject.cpp
@@ -4,4 +4,9 @@ int main()
 {
     int a = 10;
     std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
 }
